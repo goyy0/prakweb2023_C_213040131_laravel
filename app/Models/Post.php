3ed2e09865a3e0d06794803940a245760e5406fa
goyy0 <yogisaputra9319@gmail.com>
2023-11-13 02:07:45 +0700

    push code vid 7

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,33 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Igoy Disiniii",
-            "slug" => "Awas Igoy dimarii",
-            "author" => "igoy",
-            "body" => "Yah jadi begituu"
-        ],
-        [
-            "title" => "Igoy Disiniii",
-            "slug" => "Awas Igoy dimarii",
-            "author" => "igoy",
-            "body" => "Ada igoy jangan larii"
-        ]
-    ];
-
-    public static function all()
-    {
-
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
+    use HasFactory;
 }
