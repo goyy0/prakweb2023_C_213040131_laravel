8cdefb47e3aca5d98200eab9e33b99b760257151
goyy0 <yogisaputra9319@gmail.com>
2023-12-06 14:24:32 +0700

    update vid-11

--- routes/web.php
+++ routes/web.php
@@ -38,27 +38,22 @@ Route::get('/about', function () {
 
 Route::get('/blog', [PostController::class, 'index']);
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
-
 Route::get('/categories', function () {
     return view('categories', [
         'title' => 'Post Categories',
         'categories' => Category::all()
-
     ]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('category', [
-        'title' => $category->name,
-        'post' => $category->posts,
-        'category' => $category->name
-
+    return view('posts', [
+        'title' => "Post By Category : $category->name",
+        'posts' => $category->posts->load('category', 'author'),
     ]);
 });
-
 Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
-        'title' => 'User Posts',
-        'posts' => $author->posts,
+        'title' => "Post By Author : $author->name",
+        'posts' => $author->posts->load('category', 'author'),
     ]);
 });
