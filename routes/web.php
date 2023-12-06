1ec533f6ef01e1fdbe470f46da80cba6e6730b3e
goyy0 <yogisaputra9319@gmail.com>
2023-12-06 22:04:25 +0700

    upload vid-17

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,17 @@
 <?php
 
-use App\Models\Post;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
+
 use App\Http\Controllers\PostController;
+// use App\Models\Post;
+use Illuminate\Support\Facades\Route;
 use App\Models\Category;
 //use App\Models\User;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardController;
+
+use App\Http\Controllers\DashboardPostController;
 
 
 /*
@@ -58,4 +61,8 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+})->middleware('auth');
+
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
